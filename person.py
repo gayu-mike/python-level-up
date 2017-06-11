88cef9587b3999f276a28fd9b734c10d46c8c585
zxymike <zxymike93@gmail.com>
2017-06-11 12:08:33 +0800

    __repr__ in BaseClass

--- person.py
+++ person.py
@@ -1,4 +1,13 @@
-class Person:
+class ModelHelper:
+    def __repr__(self):
+        """交互提示模式下的显示重载"""
+        class_name = self.__class__.__name__
+        a = ['{}: `{}`'.format(k, v) for k, v in self.__dict__.items()]
+        attrs = '\n'.join(a)
+        return '<class %s>\n%s' % (class_name, attrs)
+
+
+class Person(ModelHelper):
     def __init__(self, name, job=None, pay=0):
         """构造函数: 初始化实例的时候自动运行"""
         self.name = name
@@ -7,7 +16,7 @@ class Person:
 
     def __str__(self):
         """print 运算符的重载"""
-        return '[Person: %s, %s]' % (self.name, self.__dict__)
+        return '[Person: %s, %s]' % (self.name, self.pay)
     
     def last_name(self):
         return self.name.split()[-1]
@@ -20,9 +29,8 @@ def test():
     bob = Person('Bob Smith')
     sue = Person('Sue Jones', job='dev', pay=100000)
     for p in [bob, sue]:
-        print(p.pay)
+        print(p)
         p.give_raise(.1)
-        print(p.last_name(), p.pay)
 
     assert bob.name == 'Bob Smith'
     assert bob.job is None
