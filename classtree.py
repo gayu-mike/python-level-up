c369bda8136b591e4de43022f4700b8e315d95e0
zxymike <zxymike93@gmail.com>
2017-06-13 01:03:39 +0800

    class tree

---
+++ classtree.py
@@ -0,0 +1,28 @@
+def classtree(cls, indent):
+    print('|' + '_'*indent + cls.__name__)
+    for supercls in cls.__bases__:
+        classtree(supercls, indent+4)
+
+
+def instancetree(ins):
+    print('\nTree of %s' % ins)
+    classtree(ins.__class__, 2)
+
+
+def test():
+    class A:    pass
+    class B(A): pass
+    class C(A): pass
+    class D(B, C):  pass
+    class E:    pass
+    class F(D, E):  pass
+    instancetree(A())
+    instancetree(B())
+    instancetree(C())
+    instancetree(D())
+    instancetree(E())
+    instancetree(F())
+
+
+if __name__ == '__main__':
+    test()
